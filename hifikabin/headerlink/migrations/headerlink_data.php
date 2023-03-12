c1b9ac70a3e81765b4f500d1b7a7a34527416ff9
cabot <cabot@users.noreply.github.com>
2023-03-12 11:32:11 +0100

    Update headerlink_data.php

--- hifikabin/headerlink/migrations/headerlink_data.php
+++ hifikabin/headerlink/migrations/headerlink_data.php
@@ -3,7 +3,7 @@
 *
 * @package phpBB Extension - Header Link
 * @copyright (c) 2015 - HiFiKabin
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
 
@@ -15,47 +15,47 @@ class headerlink_data extends \phpbb\db\migration\migration
 	public function update_schema()
 	{
 		return array(
-			'add_tables'	=> array(
+			'add_tables'	=> [
 			// ADD NEW TABLE STRUCTURE
-				$this->table_prefix . 'headerlink'	=> array(
-					'COLUMNS' => array(
-					'headerlink_id'						=> array('UINT', null, 'auto_increment'),
-					'headerlink_url'					=> array('VCHAR', ''),
-					'headerlink_name'					=> array('VCHAR', ''),
-					'headerlink_hover'					=> array('VCHAR', ''),
-					'headerlink_icon'					=> array('VCHAR', ''),
-					'headerlink_icon_colour'			=> array('VCHAR', ''),
-					'headerlink_icon_sw'				=> array('BOOL', 0),
-					'headerlink_permission'				=> array('VCHAR', ''),
-					'headerlink_target'					=> array('VCHAR', ''),
-					'headerlink_button_colour'			=> array('VCHAR', ''),
-					'headerlink_text_colour'			=> array('VCHAR', ''),
-					'headerlink_text_shadow'			=> array('VCHAR', ''),
-					'headerlink_text_shadow_colour'		=> array('VCHAR', ''),
-					),
+				$this->table_prefix . 'headerlink'	=> [
+					'COLUMNS' => [
+					'headerlink_id'						=> ['UINT', null, 'auto_increment'],
+					'headerlink_url'					=> ['VCHAR', ''],
+					'headerlink_name'					=> ['VCHAR', ''],
+					'headerlink_hover'					=> ['VCHAR', ''],
+					'headerlink_icon'					=> ['VCHAR', ''],
+					'headerlink_icon_colour'			=> ['VCHAR', ''],
+					'headerlink_icon_sw'				=> ['BOOL', 0],
+					'headerlink_permission'				=> ['VCHAR', ''],
+					'headerlink_target'					=> ['VCHAR', ''],
+					'headerlink_button_colour'			=> ['VCHAR', ''],
+					'headerlink_text_colour'			=> ['VCHAR', ''],
+					'headerlink_text_shadow'			=> ['VCHAR', ''],
+					'headerlink_text_shadow_colour'		=> ['VCHAR', ''],
+					],
 					'PRIMARY_KEY'	=> 'headerlink_id',
-				),
-			),
+				],
+			],
 		);
 	}
 
 	public function update_data()
 	{
 		// ADD CONFIG 
-		return array(
-			array('config.add', array('headerlink_enable', '0')),
-			array('config.add', array('headerlink_hover_colour', '#0c93d7')),
-			array('config.add', array('headerlink_navbar', '0')),
+		return [
+			['config.add', ['headerlink_enable', '0']],
+			['config.add', ['headerlink_hover_colour', '#0c93d7']],
+			['config.add', ['headerlink_navbar', '0']],
 
 			// Add ACP modules
-			array('module.add', array('acp', 'ACP_CAT_DOT_MODS', 'ACP_HEADERLINK_TITLE')),
-			array('module.add', array('acp', 'ACP_HEADERLINK_TITLE', array(
+			['module.add', ['acp', 'ACP_CAT_DOT_MODS', 'ACP_HEADERLINK_TITLE']],
+			['module.add', ['acp', 'ACP_HEADERLINK_TITLE', [
 				'module_basename'	=> '\hifikabin\headerlink\acp\main_module',
 				'module_langname'	=> 'ACP_HEADERLINK',
 				'module_mode'		=> 'overview',
 				'module_auth'		=> 'ext_hifikabin/headerlink && acl_a_board',
-			))),
-		);
+			]]],
+		];
 	}
 
 	/**
@@ -66,10 +66,10 @@ class headerlink_data extends \phpbb\db\migration\migration
 	*/
 	public function revert_schema()
 	{
-		return array(
-			'drop_tables'	=> array(
+		return [
+			'drop_tables'	=> [
 				$this->table_prefix . 'headerlink',
-			),
-		);
+			],
+		];
 	}
 }
