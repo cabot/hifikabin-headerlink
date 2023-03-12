648a0620672d0e62e69bd8934d1d5c4160e8cfd9
cabot <cabot@users.noreply.github.com>
2023-03-12 11:28:28 +0100

    Update listener.php

--- hifikabin/headerlink/event/listener.php
+++ hifikabin/headerlink/event/listener.php
@@ -3,19 +3,21 @@
 *
 * @package phpBB Extension - Header Link
 * @copyright (c) 2015 - HiFiKabin
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
 
 namespace hifikabin\headerlink\event;
 
-/**
-* @ignore
-*/
+use phpbb\template\template;
+use phpbb\user;
+use phpbb\db\driver\driver_interface;
+use phpbb\config\config;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
 * Event listener
+*
 */
 class listener implements EventSubscriberInterface
 {
@@ -24,6 +26,9 @@ class listener implements EventSubscriberInterface
 
 	/** @var  \phpbb\user */
 	protected $user;
+
+	/** @var \phpbb\config\config */
+	protected $config;
 
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
@@ -34,57 +39,53 @@ class listener implements EventSubscriberInterface
 	/** @var string */
 	protected $headerlink_table;
 
-	/** @var \phpbb\config\config */
-	protected $config;
-
 	/**
 	* Constructor
 	*
 	* @param \phpbb\template\template			$template
-	* @parem \phpbb\user 						$user
+	* @param \phpbb\user 						$user
 	* @param \phpbb\db\driver\driver_interface	$db
-	* @parem \phpbb\request\request				$request
-	* @param string								$headerlink_table
+	* @param \phpbb\request\request				$request
 	* @param \phpbb\config\config				$config
+	* @param string								$headerlink_table
 	*/
 
-	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db,
-	\phpbb\request\request $request, $headerlink_table, \phpbb\config\config $config)
+	public function __construct(template $template, user $user, config $config, driver_interface $db, request $request, $headerlink_table)
 	{
 		$this->template = $template;
 		$this->user = $user;
 		$this->db = $db;
 		$this->request = $request;
-		$this->headerlink_table = $headerlink_table;
 		$this->config = $config;
+		$this->headerlink_table = $headerlink_table;
 	}
 
 	public static function getSubscribedEvents()
 	{
-		return array(
+		return [
 			'core.user_setup'	=> 'load_language_on_setup',
 			'core.page_header'	=> 'headerlink',
-		);
+		];
 	}
 
 	public function load_language_on_setup($event)
 	{
 		$lang_set_ext		= $event['lang_set_ext'];
-		$lang_set_ext[]		= array(
+		$lang_set_ext[]		= [
 			'ext_name'		=> 'hifikabin/headerlink',
 			'lang_set'		=> 'common_headerlink',
-		);
+		];
 		$event['lang_set_ext'] = $lang_set_ext;
 	}
 
 	public function headerlink($event)
 	{
-		$this->template->assign_vars(array(
+		$this->template->assign_vars([
 			'HEADERLINK_ENABLE'					=> $this->config['headerlink_enable'],
 			'HEADERLINK_HOVER_COLOUR'			=> $this->config['headerlink_hover_colour'],
 			'HEADERLINK_NAVBAR'					=> $this->config['headerlink_navbar'],
 			'HEADERLINK_ALIGN'					=> $this->config['headerlink_align'],
-			));
+		]);
 
 		$sql = 'SELECT * 
 		FROM '. $this->headerlink_table;
@@ -95,7 +96,7 @@ class listener implements EventSubscriberInterface
 			{
 			if (!empty($row['headerlink_url']))
 				{
-				$this->template->assign_block_vars('headerlink_urls', array(
+				$this->template->assign_block_vars('headerlink_urls', [
 					'HEADERLINK_URL'					=> $row['headerlink_url'],
 					'HEADERLINK_NAME'					=> $row['headerlink_name'],
 					'HEADERLINK_HOVER'					=> $row['headerlink_hover'],
@@ -108,7 +109,7 @@ class listener implements EventSubscriberInterface
 					'HEADERLINK_TEXT_COLOUR'			=> $row['headerlink_text_colour'],
 					'HEADERLINK_TEXT_SHADOW'			=> $row['headerlink_text_shadow'],
 					'HEADERLINK_TEXT_SHADOW_COLOUR'		=> $row['headerlink_text_shadow_colour'],
-					));
+					]);
 				}
 			}
 		$this->db->sql_freeresult($result);
