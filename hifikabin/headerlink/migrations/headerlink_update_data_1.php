5ab032888cfb0c923d5d3503108664d3eba98b2d
cabot <cabot@users.noreply.github.com>
2023-03-12 11:32:42 +0100

    Update headerlink_update_data_1.php

--- hifikabin/headerlink/migrations/headerlink_update_data_1.php
+++ hifikabin/headerlink/migrations/headerlink_update_data_1.php
@@ -3,7 +3,7 @@
 *
 * @package phpBB Extension - Header Link
 * @copyright (c) 2015 - HiFiKabin
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
 
@@ -13,14 +13,14 @@ class headerlink_update_data_1 extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
-		return array('\hifikabin\headerlink\migrations\headerlink_data');
+		return ['\hifikabin\headerlink\migrations\headerlink_data'];
 	}
 
 	public function update_data()
 	{
-		return array(
+		return [
 			// Add config
-			array('config.add', array('headerlink_align', '0')),
-			);
+			['config.add', ['headerlink_align', '0']],
+		];
 	}
 }
