a1f09dc3c90d72be04d7b91caca5b838f1e63b03
cabot <cabot@users.noreply.github.com>
2023-05-19 12:24:44 +0200

    Cleaning and formatting

--- hifikabin/headerlink/ext.php
+++ hifikabin/headerlink/ext.php
@@ -1,25 +1,25 @@
 <?php
 /**
-*
-* @package phpBB Extension - Header Link
-* @copyright (c) 2015 - HiFiKabin
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-*/
+ *
+ * @package phpBB Extension - Header Link
+ * @copyright (c) 2015 - HiFiKabin
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
 namespace hifikabin\headerlink;
 
 /**
-* Extension class for custom enable/disable/purge actions
-*/
+ * Extension class for custom enable/disable/purge actions
+ */
 class ext extends \phpbb\extension\base
 {
 	/**
-	* Enable extension if phpBB version requirement is met
-	*
-	* @return bool
-	* @access public
-	*/
+	 * Enable extension if phpBB version requirement is met
+	 *
+	 * @return bool
+	 * @access public
+	 */
 	public function is_enableable()
 	{
 		return phpbb_version_compare(PHPBB_VERSION, '3.2.0@dev', '>=');
